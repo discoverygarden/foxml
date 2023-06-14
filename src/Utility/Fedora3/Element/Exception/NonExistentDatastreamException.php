a9202005e58a0d887a668e086e40d087044c1ec1
Adam Vessey <adam@discoverygarden.ca>
2023-06-14 18:42:07 -0300

    Coding standards.

--- src/Utility/Fedora3/Element/Exception/NonExistentDatastreamException.php
+++ src/Utility/Fedora3/Element/Exception/NonExistentDatastreamException.php
@@ -2,18 +2,36 @@
 
 namespace Drupal\foxml\Utility\Fedora3\Element\Exception;
 
+/**
+ * Exception for flow control; thrown if we require an absent datastream.
+ */
 class NonExistentDatastreamException extends \Exception {
+  /**
+   * The PID of the object missing the datastream.
+   *
+   * @var mixed
+   */
+  protected $pid;
+
+  /**
+   * The ID of the missing datastream.
+   *
+   * @var string
+   */
+  protected string $datastreamId;
 
   /**
    * Constructor.
    *
-   * @param mixed $PID
+   * @param mixed $pid
+   *   The PID that's missing the datastream.
    * @param string $datastream_id
+   *   The datastream that's missing.
    */
-  public function __construct($PID, string $datastream_id) {
-    parent::__construct("Could not find $PID/$datastream_id");
-    $this->pid = $PID;
-    $this->dsid = $datastream_id;
+  public function __construct($pid, string $datastream_id) {
+    parent::__construct("Could not find $pid/$datastream_id");
+    $this->pid = $pid;
+    $this->datastreamId = $datastream_id;
   }
 
 }
