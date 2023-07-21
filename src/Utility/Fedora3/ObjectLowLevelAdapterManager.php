8548c70f6b8ee440be0a0e0e82d4c56d07b6a7b7
Adam Vessey <adam@discoverygarden.ca>
2023-07-21 14:55:59 -0300

    Better log message and less breaking when un-configured.

--- src/Utility/Fedora3/ObjectLowLevelAdapterManager.php
+++ src/Utility/Fedora3/ObjectLowLevelAdapterManager.php
@@ -2,10 +2,29 @@
 
 namespace Drupal\foxml\Utility\Fedora3;
 
+use Drupal\foxml\Utility\Fedora3\Exceptions\NotImplementedException;
+use Psr\Log\LoggerInterface;
+
 /**
  * Object low-level adapter service collector.
  */
 class ObjectLowLevelAdapterManager extends AbstractLowLevelAdapterManager implements ObjectLowLevelAdapterInterface {
+
+  /**
+   * Logger service.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected LoggerInterface $logger;
+
+  /**
+   * Constructor.
+   */
+  public function __construct(
+    LoggerInterface $logger
+  ) {
+    $this->logger = $logger;
+  }
 
   /**
    * {@inheritdoc}
@@ -24,7 +43,43 @@ class ObjectLowLevelAdapterManager extends AbstractLowLevelAdapterManager implem
   public function getIterator() : \Traversable {
     $this->valid();
 
-    return reset($this->validAdapters);
+    if ($first_valid_adapter = reset($this->validAdapters)) {
+      return $first_valid_adapter;
+    }
+    else {
+      $this->logger->notice('No valid low-level FOXML object adapter found; configure as necessary, if relevant.');
+      return new class() implements ObjectLowLevelAdapterInterface {
+
+        /**
+         * {@inheritDoc}
+         */
+        public function dereference($id) : string {
+          throw new NotImplementedException('Attempting to dereference from an invalid adapter.');
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public function getIterator() : \Traversable {
+          return new \EmptyIterator();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public function getIteratorType() : int {
+          return ObjectLowLevelAdapterInterface::ITERATOR_PID;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public function valid() : bool {
+          return FALSE;
+        }
+
+      };
+    }
   }
 
   /**
